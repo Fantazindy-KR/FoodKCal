a7c803b07326e59e48b0c3beab1a6f0519771887
Fantazindy-KR <leehyen4965@gmail.com>
2022-02-02 15:13:57 +0900

    Load Data from API

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import FoodList from "./FoodList";
-import mockItems from "../mock.json";
 import { useState } from "react";
+import { getFoods } from "../api";
 
 function App() {
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -15,11 +15,17 @@ function App() {
     setItems(nextItems);
   };
 
+  const handleLoadClick = async () => {
+    const { foods } = await getFoods();
+    setItems(foods);
+  };
+
   return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleKCalClick}>칼로리순</button>
       <FoodList items={sortedItems} onDelete={handleDelete} />
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
